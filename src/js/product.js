ceb757770e26437383b84baa30d8eeb1ac1fbcda
Marcelo Patricio <marcelo@patricio.net.br>
2026-05-18 20:56:26 -0300

    W02 Team Activity until URL Parameters item 3

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { setLocalStorage, getLocalStorage } from './utils.mjs'; // add getLocalStorage to retrieve existing items on the cart
+import { setLocalStorage, getLocalStorage, getParam } from './utils.mjs'; // add getLocalStorage to retrieve existing items on the cart
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
