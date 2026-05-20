6bab8e0605023decb6459edc97cc65a76cb23197
Marcelo Patricio <marcelo@patricio.net.br>
2026-05-20 18:52:25 -0300

    move images and json to public; create ProductList and render the index product list dynamically

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,31 @@
+function productCardTemplate(product) {
+    return `
+    <li class="product-card">
+        <a href="product_pages/?product=${product.Id}">
+        <img src="${product.Image} " alt="${product.Name} " />
+        <h3 class="card__brand">${product.Brand.Name}</h3>
+        <h2 class="card__name">${product.NameWithoutBrand}</h2>
+        <p class="product-card__price">$${product.ListPrice}</p>
+        </a>
+    </li>`;
+}
+
+export default class ProductList {
+    constructor(category, dataSource, listElement) {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+    renderList(list) {
+        // pass every product in the template and creat a list of HTML's
+        const htmlStrings = list.map(productCardTemplate);
+        // join the HTML pieces and inject in the element (<ul>)
+        this.listElement.insertAdjacentHTML('afterbegin', htmlStrings.join(''));
+    }
+
+    async init() {
+        const list = await this.dataSource.getData();
+        // call renderList passing the list
+        this.renderList(list);
+    }
+}
---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+import ProductData from './ProductData.mjs';
+import ProductList from './ProductList.mjs';
+
+// create the data source (JSON connectio)
+const dataSource = new ProductData('tents');
+// render the product list (output target)
+const listElement = document.querySelector('.product-list');
+// new instance of ProductList
+const productList = new ProductList('tents', dataSource, listElement);
+
+// initialize the work! data fill and render on the page.
+productList.init();
