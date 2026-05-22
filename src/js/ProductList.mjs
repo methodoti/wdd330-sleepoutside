a5f316aca1c636cc7df938623b496815c8df3dec
Marcelo Patricio <marcelo@patricio.net.br>
2026-05-22 18:31:16 -0300

    add discount indicator to product listing

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,13 +1,27 @@
 import { renderListWithTemplate } from './utils.mjs';
 
 function productCardTemplate(product) {
+    // initialize variable for the visual indicator 
+    let discountIndicator = "";
+
+    // is there discount?
+    if (product.FinalPrice < product.SuggestedRetailPrice) {
+        // flag as discounted with %
+        const discountAmount = product.SuggestedRetailPrice - product.FinalPrice;
+        const discountPercentage = Math.round((discountAmount / product.SuggestedRetailPrice) * 100);
+
+        // html with the flag
+        discountIndicator = `<p class="discount-badge">Save ${discountPercentage}%</p>`;
+    }
+
     return `
     <li class="product-card">
         <a href="product_pages/?product=${product.Id}">
+        ${discountIndicator}
         <img src="${product.Image} " alt="${product.Name} " />
         <h3 class="card__brand">${product.Brand.Name}</h3>
         <h2 class="card__name">${product.NameWithoutBrand}</h2>
-        <p class="product-card__price">$${product.ListPrice}</p>
+        <p class="product-card__price">$${product.FinalPrice}</p>
         </a>
     </li>`;
 }
