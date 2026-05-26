0887362cf7a43317332de31462dcee458c401afd
Marcelo Patricio <marcelo@patricio.net.br>
2026-05-26 17:44:30 -0300

    refactor: update suggested retail indicator logic

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,20 +1,28 @@
 import { renderListWithTemplate } from './utils.mjs';
 
 function productCardTemplate(product) {
-    // initialize variable for the visual indicator 
-    let discountIndicator = "";
+  // initialize variable for the visual indicator
+  let discountIndicator = '';
+  let suggestedRetailIndicator = '';
 
-    // is there discount?
-    if (product.FinalPrice < product.SuggestedRetailPrice) {
-        // flag as discounted with %
-        const discountAmount = product.SuggestedRetailPrice - product.FinalPrice;
-        const discountPercentage = Math.round((discountAmount / product.SuggestedRetailPrice) * 100);
+  // is there discount?
+  if (product.FinalPrice < product.SuggestedRetailPrice) {
+    // flag as discounted with %
+    const discountAmount = product.SuggestedRetailPrice - product.FinalPrice;
+    // const discountPercentage = Math.round((discountAmount / product.SuggestedRetailPrice) * 100);
+    const suggestedPrice = product.SuggestedRetailPrice;
+    const discountPercentage = Math.round(
+      (discountAmount / suggestedPrice) * 100,
+    );
 
-        // html with the flag
-        discountIndicator = `<p class="discount-badge">Save ${discountPercentage}%</p>`;
-    }
+    // html with the flag
+    discountIndicator = `<p class="discount-badge">Save ${discountPercentage}%</p>`;
 
-    return `
+    // suggested retail price
+    suggestedRetailIndicator = `<p class="suggested__price"><del>$${product.SuggestedRetailPrice}</del></p>`;
+  }
+
+  return `
     <li class="product-card">
         <a href="product_pages/?product=${product.Id}">
         ${discountIndicator}
@@ -22,30 +30,30 @@ function productCardTemplate(product) {
         <h3 class="card__brand">${product.Brand.Name}</h3>
         <h2 class="card__name">${product.NameWithoutBrand}</h2>
         <p class="product-card__price">$${product.FinalPrice}</p>
-        <p class="suggested__price"><del>$${product.SuggestedRetailPrice}</del></p>
+        ${suggestedRetailIndicator}
         </a>
     </li>`;
 }
 
 export default class ProductList {
-    constructor(category, dataSource, listElement) {
-        this.category = category;
-        this.dataSource = dataSource;
-        this.listElement = listElement;
-    }
-    renderList(list) {
-        // pass every product in the template and creat a list of HTML's
-        //const htmlStrings = list.map(productCardTemplate);
-        // join the HTML pieces and inject in the element (<ul>)
-        //this.listElement.insertAdjacentHTML('afterbegin', htmlStrings.join(''));
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
+  renderList(list) {
+    // pass every product in the template and creat a list of HTML's
+    //const htmlStrings = list.map(productCardTemplate);
+    // join the HTML pieces and inject in the element (<ul>)
+    //this.listElement.insertAdjacentHTML('afterbegin', htmlStrings.join(''));
 
-        // call the new universal tool passing: the template, the <ul>, and the list
-        renderListWithTemplate(productCardTemplate, this.listElement, list);
-    }
+    // call the new universal tool passing: the template, the <ul>, and the list
+    renderListWithTemplate(productCardTemplate, this.listElement, list);
+  }
 
-    async init() {
-        const list = await this.dataSource.getData();
-        // call renderList passing the list
-        this.renderList(list);
-    }
+  async init() {
+    const list = await this.dataSource.getData();
+    // call renderList passing the list
+    this.renderList(list);
+  }
 }
